4edaddb9c4330692170e5236b98710464f71693f
Wei Wei <weiwei@geekteahouse.cn>
2022-03-15 11:08:23 +0800

    Use the new @ts-monad/observable

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,5 +1,14 @@
-import { mutable } from "@ts-monad/observable";
-import { property, propertyOf } from "..";
+import { fmap, mutable } from "@ts-monad/observable";
+import {
+  chain,
+  currentValue,
+  debounce,
+  delay,
+  noop,
+  property,
+  propertyOf,
+  throttle,
+} from "..";
 
 describe("#property & #propertyOf", () => {
   it("should extract a property of observable object", () => {
@@ -28,6 +37,143 @@ describe("#property & #propertyOf", () => {
     // Cascaded unobserve
     obX.unobserve();
     obY.unobserve();
-    expect(obj.isObserved()).toBe(false);
+    expect(obj.isObserved()).toBeFalsy();
+  });
+});
+
+describe("currentValue", () => {
+  it("should get the current value of the observable", () => {
+    const mut = mutable(0);
+
+    expect(currentValue(mut)).toBe(0);
+    expect(mut.isObserved()).toBeFalsy();
+
+    mut.update((i) => i + 1);
+
+    expect(currentValue(mut)).toBe(1);
+    expect(mut.isObserved()).toBeFalsy();
+  });
+});
+
+describe("throttle", () => {
+  it("should limit the frequency of update calls", async () => {
+    const mut = mutable(0);
+    const obT = throttle(10)(mut);
+    const cb = jest.fn();
+    const obn = obT.observe(cb);
+
+    expect(obn.value).toBe(0);
+
+    mut.update((i) => i + 1); // 1
+    expect(cb).not.toBeCalled();
+
+    await new Promise((r) => setTimeout(r, 15));
+    expect(cb).toHaveBeenCalledTimes(1);
+    expect(cb).toHaveBeenLastCalledWith(1);
+
+    await new Promise((r) => setTimeout(r, 15));
+    mut.update((i) => i + 1); // 2
+    mut.update((i) => i + 1); // 3
+    mut.update((i) => i + 1); // 4
+    expect(cb).toHaveBeenCalledTimes(2);
+    expect(cb).toHaveBeenLastCalledWith(2);
+
+    await new Promise((r) => setTimeout(r, 15));
+    expect(cb).toHaveBeenCalledTimes(3);
+    expect(cb).toHaveBeenLastCalledWith(4);
+
+    obn.unobserve();
+    expect(obT.isObserved()).toBeFalsy();
+    expect(mut.isObserved()).toBeFalsy();
+  });
+});
+
+describe("debounce", () => {
+  it("should hold the update until it is stable", async () => {
+    const mut = mutable(0);
+    const obD = debounce(10)(mut);
+    const cb = jest.fn();
+    const obn = obD.observe(cb);
+
+    expect(obn.value).toBe(0);
+
+    mut.update((i) => i + 1); // 1
+    expect(cb).not.toBeCalled();
+
+    await new Promise((r) => setTimeout(r, 15));
+    expect(cb).toHaveBeenCalledTimes(1);
+    expect(cb).toHaveBeenLastCalledWith(1);
+
+    await new Promise((r) => setTimeout(r, 15));
+    mut.update((i) => i + 1); // 2
+    mut.update((i) => i + 1); // 3
+    mut.update((i) => i + 1); // 4
+    expect(cb).toHaveBeenCalledTimes(1);
+
+    await new Promise((r) => setTimeout(r, 15));
+    expect(cb).toHaveBeenCalledTimes(2);
+    expect(cb).toHaveBeenLastCalledWith(4);
+
+    obn.unobserve();
+    expect(obD.isObserved()).toBeFalsy();
+    expect(mut.isObserved()).toBeFalsy();
+  });
+});
+
+describe("delay", () => {
+  it("should delay the updates", async () => {
+    const mut = mutable(0);
+    const ob = delay(10)(mut);
+    const cb = jest.fn();
+    const obn = ob.observe(cb);
+
+    expect(obn.value).toBe(0);
+
+    mut.update((i) => i + 1); // 1
+    expect(cb).not.toBeCalled();
+
+    await new Promise((r) => setTimeout(r, 15));
+    expect(cb).toHaveBeenCalledTimes(1);
+    expect(cb).toHaveBeenLastCalledWith(1);
+
+    obn.unobserve();
+    expect(ob.isObserved()).toBeFalsy();
+    expect(mut.isObserved()).toBeFalsy();
+  });
+});
+
+describe("chain", () => {
+  it("should be able to create an observable with chained operators", () => {
+    const mutN = mutable(0);
+    const mutF = mutable((x: number) => x * 3);
+    const obM = chain(mutN)
+      .fmap((x) => x * 2) // n * 2
+      .fmap((x) => x + 1) // n * 2 + 1
+      .bind((x) => fmap((f: (n: number) => number) => f(x))(mutF)) // f(n * 2 + 1)
+      .observable(); // m = (n * 2 + 1) * 3
+
+    const cb = jest.fn();
+    const { value, unobserve } = obM.observe(cb);
+
+    expect(value).toBe(3);
+
+    mutN.update((i) => i + 1); // n is 1
+
+    expect(cb).toHaveBeenLastCalledWith(9);
+
+    mutF.update((f) => (x) => f(x) - x - 1); // m = (n * 2 + 1) * 2 - 1
+
+    expect(cb).toHaveBeenLastCalledWith(5);
+
+    unobserve();
+    expect(obM.isObserved()).toBeFalsy();
+    expect(mutN.isObserved()).toBeFalsy();
+    expect(mutF.isObserved()).toBeFalsy();
+  });
+});
+
+describe("__DUMMY_TEST__", () => {
+  test("ensure the helper functions are covered", () => {
+    expect(noop()).toBeUndefined();
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
-import { fmap, Observable } from "@ts-monad/observable";
+import {
+  bind,
+  fmap,
+  observable,
+  Observable,
+  Observer,
+} from "@ts-monad/observable";
+
+export const noop = () => {};
 
 export const property = <R>(key: keyof R) => fmap((o: R) => o[key]);
 
@@ -6,3 +14,67 @@ export const propertyOf =
   <R>(ob: Observable<R>) =>
   (key: keyof R) =>
     property(key)(ob);
+
+export const currentValue = <T>(ob: Observable<T>): T => {
+  const { value, unobserve } = ob.observe(noop);
+  unobserve();
+  return value;
+};
+
+export const tmap =
+  <T>(f: (observer: Observer<T>) => Observer<T>) =>
+  (ob: Observable<T>): Observable<T> =>
+    observable((observer) => ob.observe(f(observer)));
+
+export const throttle = (ms: number) =>
+  tmap((observer) => {
+    let timestamp = Date.now();
+    let timeout: ReturnType<typeof setTimeout> | null = null;
+
+    return (newValue) => {
+      const notify = () => {
+        if (timeout) {
+          clearTimeout(timeout);
+          timeout = null;
+        }
+
+        const now = Date.now();
+        if (now > timestamp + ms) {
+          timestamp = now;
+          observer(newValue);
+        } else {
+          timeout = setTimeout(notify, timestamp + ms - now);
+        }
+      };
+      notify();
+    };
+  });
+
+export const debounce = (ms: number) =>
+  tmap((observer) => {
+    let timeout: ReturnType<typeof setTimeout> | null = null;
+    return (newValue) => {
+      if (timeout) {
+        clearTimeout(timeout);
+      }
+      timeout = setTimeout(() => {
+        timeout = null;
+        observer(newValue);
+      }, ms);
+    };
+  });
+
+export const delay = (ms: number) =>
+  tmap((observer) => (newValue) => setTimeout(() => observer(newValue), ms));
+
+export type ObservableChain<S> = {
+  fmap: <T>(f: (s: S) => T) => ObservableChain<T>;
+  bind: <T>(f: (s: S) => Observable<T>) => ObservableChain<T>;
+  observable: () => Observable<S>;
+};
+
+export const chain = <S>(ob: Observable<S>): ObservableChain<S> => ({
+  fmap: (f) => chain(fmap(f)(ob)),
+  bind: (f) => chain(bind(f)(ob)),
+  observable: () => ob,
+});
